f0ffe30d4b6f0132ab68578113409c6919e85ef7
VedasreeRaja <130262363+VedasreeRaja@users.noreply.github.com>
2023-09-23 02:12:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,7 +173,8 @@ if __name__ == '__main__':
             rouge_l_scores.append(rouge_l_score)
             sum2 += rouge_l_score
         average_rouge_l = sum2 / len(rouge_l_scores)
-        st.write(f"Average ROUGE-L Score (F1) for {len(reference_texts)} reference sentences: {average_rouge_l:.4f}")
+        s3 =  (f"Average ROUGE-L Score (F1) for {len(reference_texts)} reference sentences: {average_rouge_l:.4f}")
+        st.markdown(s3)
         #for reference_text in reference_texts:
         #       rouge_l_score = rouge_l_f1(hypothesis_text, reference_text)
         #      s3 = ('**ROUGE-L Score -> {:.4f}**'.format(rouge_l_score))
