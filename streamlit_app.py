273475eb1f7dc8718c71eb1b2936a3d920b6ac95
VedasreeRaja <130262363+VedasreeRaja@users.noreply.github.com>
2023-09-23 01:42:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,7 +165,8 @@ if __name__ == '__main__':
             hypothesis_text = predicted_caption
             for reference_text in reference_texts:
                 rouge_l_score = rouge_l_f1(hypothesis_text, reference_text)
-                print(f"ROUGE-L Score (F1) for Reference: {rouge_l_score:.4f}")
+                s3 = "ROUGE-L Score (F1) for Reference: {rouge_l_score:.4f}"
+                st.markdown(s3)
         average = sum/5
         s2 = ('**Average BLEU score -> {:.4f}**'.format(average))
         st.markdown(s2)
